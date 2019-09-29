a7e4e5a086cef80b18f9897d621e2a58414d0869
Jessica <jessica.archerd@gmail.com>
2019-09-28 19:39:10 -0500

    altering file structure to allow for hosting

---
+++ mystery-book-club/POJOS/Author.java
@@ -0,0 +1,46 @@
+package com.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Author {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private long authorID;
+	private String firstName;
+	private String lastName;
+	
+	public Author() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public long getAuthorID() {
+		return authorID;
+	}
+
+	public void setAuthorID(long authorID) {
+		this.authorID = authorID;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+}
