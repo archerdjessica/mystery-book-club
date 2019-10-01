efa507994a423190f0f1b1f7e78e2788ef062570
Chris <chrisadams@letu.edu>
2019-10-01 07:52:18 -0500

    admin add publisher v2

--- mystery-book-club/src/app/app-routing.module.ts
+++ mystery-book-club/src/app/app-routing.module.ts
@@ -7,10 +7,13 @@ import { AppComponent }         from './app.component';
 import { BrowserModule }  from '@angular/platform-browser';
 
 import { HttpClientModule }    from '@angular/common/http';
-
+import { AdminAddBookComponent} from './admin-add-book/admin-add-book.component';
+import { AdminAddPublisherComponent} from './admin-add-publisher/admin-add-publisher.component';
 
 const routes: Routes = [
-{path: 'addbook', component: UserAddBookToListComponent} 
+{path: 'addbook', component: UserAddBookToListComponent}, 
+{path: 'adminaddbook', component: AdminAddBookComponent},
+{path: 'addPublisher', component: AdminAddPublisherComponent},
  ];
 
 @NgModule({
