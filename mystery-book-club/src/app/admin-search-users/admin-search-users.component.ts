178d027d2a72ea4ead503795a3c751ad5c916b9a
Jessica <jessica.archerd@gmail.com>
2019-10-01 20:07:42 -0500

    search users functions  not yet completed

--- mystery-book-club/src/app/admin-search-users/admin-search-users.component.ts
+++ mystery-book-club/src/app/admin-search-users/admin-search-users.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import{User} from '../user';
+import { Component, OnInit} from '@angular/core';
+import { User } from '../user';
 import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-admin-search-users',
@@ -9,22 +10,36 @@ import { Observable } from 'rxjs';
 })
 export class AdminSearchUsersComponent implements OnInit {
 
-  users:User[]=[
-    {"id":0, "name":"Sam", "email":"SamIsGreat@email.com", "birthday":"Dec31 1989", "readingList":[], "password":"pw"},
-    {"id":1, "name":"Jessica", "email":"jessicat@yeet.yo", "birthday":"28 Feb 1996", "readingList":[], "password":"pw"},
+  users: User[] = [
+    { "id": 0, "name": "Sam", "email": "SamIsGreat@email.com", "birthday": "Dec31 1989", "readingList": [], "password": "" },
+    { "id": 1, "name": "Jessica", "email": "jessicat@yeet.yo", "birthday": "28 Feb 1996", "readingList": [], "password": "" },
   ]
 
-  constructor() {
-   }
+  result: User[];
+
+
+  constructor() { }
+
 
   ngOnInit() {
   }
 
-  public editUser(user:User){
-    console.log("selected: edit user: "+user);
+  public editUser(user: User) {
+    console.log("selected: edit user: " + user);
   };
 
+  public clear(){
+    this.result=[];
+  }
 
+  public showAll() {
+    this.search("all", null);
+  }
 
+  public search(searchBy: string, query: any) {
+
+    (searchBy == "all") ? (this.result = this.users) : (this.result = []);
+  }
 
 }
+
