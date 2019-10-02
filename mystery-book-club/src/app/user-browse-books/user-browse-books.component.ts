68e97f77fdaf3b3ed767cca6ec8432d33736df14
Chris <chrisadams@letu.edu>
2019-10-01 20:13:03 -0500

    not sure anything new

--- mystery-book-club/src/app/user-browse-books/user-browse-books.component.ts
+++ mystery-book-club/src/app/user-browse-books/user-browse-books.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-user-browse-books',
   templateUrl: './user-browse-books.component.html',
   styleUrls: ['./user-browse-books.component.css']
 })
 export class UserBrowseBooksComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit() {
