440b553028425f3d2460b816baafc6f8c2e08ffa
Jessica Archerd <52927623+archerdjessica@users.noreply.github.com>
2019-10-04 23:38:24 -0500

    Revert "Test/admin/read approve edit delete reviews"

---
+++ mystery-book-club/src/app/admin-search-users/admin-search-users.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-search-users',
+  templateUrl: './admin-search-users.component.html',
+  styleUrls: ['./admin-search-users.component.css']
+})
+export class AdminSearchUsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
